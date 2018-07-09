2558184298f90ee2229b012e21bb417580781349
Simon Johansson <simon.johansson@assaabloy.com>
2018-07-09 10:34:20 +0200

    Add binson_light.h

---
+++ binson_light.h
@@ -0,0 +1,29 @@
+#ifndef _binson_light_H_
+#define _binson_light_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file binson_light.h.h
+ *
+ * Description
+ *
+ */
+
+/*======= Includes ==========================================================*/
+
+#include "binson_parser.h"
+#include "binson_writer.h"
+
+/*======= Public macro definitions ==========================================*/
+/*======= Type Definitions and declarations =================================*/
+/*======= Public variable declarations ======================================*/
+/*======= Public function declarations ======================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _binson_light_H_ */
