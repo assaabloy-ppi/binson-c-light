edd4f2d9a66e997f2dc95aa005c829b0e62e6e77
Simon Johansson <simon.johansson@assaabloy.com>
2019-05-15 17:51:27 +0200

    Remove coverage stuff from build.sj

--- build.sh
+++ build.sh
@@ -5,14 +5,4 @@ cd DebugSan && \
 cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On -DSANITIZE_UNDEFINED=On -DBUILD_TESTS=ON -DENABLE_COVERAGE=ON ..  && \
 make -j8 && \
 ctest -j8 && \
-cd ..  && \
-mkdir -p coverage  && \
-lcov -b . \
-    -d DebugSan/src/CMakeFiles/binson_parser.dir \
-    -d DebugSan/src/CMakeFiles/binson_writer.dir \
-    -d DebugSan/src/CMakeFiles/binson_class.dir \
-    --no-recursion \
-    --no-external \
-    --capture --output-file coverage/coverage.info  && \
-genhtml -o coverage coverage/coverage.info  && \
 exit 0 || exit -1
